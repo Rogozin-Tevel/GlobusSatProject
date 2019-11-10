2b42b1ebb0312b63f3ffc78a781d6b6c4852bf3e
kariander1 <34168638+kariander1@users.noreply.github.com>
2019-11-10 16:04:13 +0200

    Update SatCommandHandler.c

--- GlobusSatProject/src/SubSystemModules/Communication/SatCommandHandler.c
+++ GlobusSatProject/src/SubSystemModules/Communication/SatCommandHandler.c
@@ -15,6 +15,28 @@ typedef struct __attribute__ ((__packed__)) delayed_cmd_t
 
 int ClearDelayedCMD_FromBuffer(unsigned int start_addr, unsigned int end_addr)
 {
+	if (end_addr <= start_addr || end_addr >= FRAM_MAX_ADDRESS) {
+		return -1;
+	}
+	unsigned int region_size = (end_addr - start_addr) * sizeof(unsigned char);
+
+	unsigned char* temp_arr = (unsigned char*)malloc(region_size);
+
+	memset(temp_arr, 0, region_size);
+
+	FRAM_write(temp_arr, start_addr, end_addr);
+
+	free(temp_arr);
+
+	unsigned char frame_count = 0;
+	FRAM_read(&frame_count, DELAYED_CMD_FRAME_COUNT_ADDR,
+		DELAYED_CMD_FRAME_COUNT_SIZE);
+
+	frame_count--;
+
+	FRAM_write(&frame_count, DELAYED_CMD_FRAME_COUNT_ADDR,
+		DELAYED_CMD_FRAME_COUNT_SIZE);
+
 	return 0;
 }
 
@@ -65,7 +87,25 @@ int ParseDataToCommand(unsigned char * data,sat_packet_t *cmd)
 int AssmbleCommand(unsigned char *data, unsigned int data_length, char type,
 		char subtype, unsigned int id, sat_packet_t *cmd)
 {
-	return 0;
+	if (NULL == cmd) {
+		return cmd_null_pointer_error;
+	}
+	cmd->ID = id;
+	cmd->cmd_type = type;
+	cmd->cmd_subtype = subtype;
+	cmd->length = 0;
+
+	if (NULL != data) {
+		unsigned int size = (data_length > MAX_COMMAND_DATA_LENGTH) ?
+			MAX_COMMAND_DATA_LENGTH : data_length;
+		cmd->length = size;
+		void* err = memcpy(cmd->data, data, data_length);
+
+		if (NULL == err) {
+			return cmd_execution_error;
+		}
+	}
+	return cmd_command_succsess;
 }
 
 // checks if a cmd time is valid for execution -> execution time has passed and command not expired
