bfaf3c091dd7b966e7ce618aeaa7fc73080dc57f
TheBlankUnknown <golanido2@gmail.com>
2019-11-13 16:11:31 +0200

    Fixed AckHandler.c
    By Maor and Yanir

--- GlobusSatProject/src/SubSystemModules/Communication/AckHandler.c
+++ GlobusSatProject/src/SubSystemModules/Communication/AckHandler.c
@@ -5,17 +5,45 @@
 #include "AckHandler.h"
 
 int SendAckPacket(ack_subtype_t acksubtype, sat_packet_t *cmd,
-		unsigned char *data, unsigned int length)
+		unsigned char *data, unsigned int length)  // Check needed...
 {
-	return 0;
+	sat_packet_t packet;
+
+	if (cmd == NULL)
+	{
+		packet.ID = 0; // Perhaps?
+	}
+	else
+	{
+		packet.ID = cmd->ID;
+	}
+	packet.cmd_type = ack_type;
+	packet.cmd_subtype = acksubtype;
+	packet.data = data;
+	packet.length = length;
+
+	return TransmitSplPacket(&packet, NULL);
 }
 
 void SendErrorMSG(ack_subtype_t fail_subt, ack_subtype_t succ_subt,
 		sat_packet_t *cmd, int err)
 {
+	if (err == 0)
+	{
+		SendAckPacket(succ_subt, cmd, cmd->data, cmd->length); //must be checked befor used!!!!
+	}
+	else
+	{
+		SendAckPacket(fail_subt, cmd, cmd->data, cmd->length); //must be checked befor used!!!!
+	}
 }
 
 void SendErrorMSG_IfError(ack_subtype_t subtype, sat_packet_t *cmd, int err)
 {
-}
+	if (err == 0)
+	{
+		return;
+	}
 
+	SendErrorMSG(subtype, subtype, cmd, err);
+}
