1e411ede4beabf53addffdb3c710c40396ece33d
maorgershman <mgmaorvids@gmail.com>
2019-12-07 16:43:57 +0200

    Fixed an error and removed redundant stuff

--- GlobusSatProject/src/SubSystemModules/Maintenance/Maintenance.c
+++ GlobusSatProject/src/SubSystemModules/Maintenance/Maintenance.c
@@ -20,121 +20,105 @@
 
 #define SECONDS_IN_1_DAY (60 * 60 * 24)
 
-inline Boolean weak_assert(Boolean condition, const char *errMsg)
-{
-	if (!condition)
-	{
-		printf(errMsg);
-	}
-	return condition;
-}
+// Maor 7.12.2019:
+// The error "handling" might be redundant, so I have removed it out meanwhile.
 
-Boolean CheckExecutionTime(time_unix prev_time, time_unix period) // Fixed by Maor & Yanir
+Boolean CheckExecutionTime(time_unix prev_time, time_unix period)
 {
 	time_unix current_time;
-
-	weak_assert(Time_getUnixEpoch(&current_time) == 0,
-			"Error in CheckExecutionTime: Can't load the current time!\n");
-
+	Time_getUnixEpoch(&current_time);
 	return current_time - prev_time > period;
 }
 
-Boolean CheckExecTimeFromFRAM(unsigned int fram_time_addr, time_unix period) // Fixed by Maor & Yanir
+Boolean CheckExecTimeFromFRAM(unsigned int fram_time_addr, time_unix period)
 {
 	time_unix prev_time;
-
-	weak_assert(FRAM_read((unsigned char*)&prev_time, fram_time_addr, sizeof(time_unix)) == 0,
-			"Error in CheckExecTimeFromFRAM: Can't load the previous execution time from the FRAM!\n");
-
-	return CheckExecutionTime(prev_time, period); // Return TRUE if the deadline has passed, FALSE otherwise.
+	FRAM_read(&prev_time, fram_time_addr, sizeof(time_unix));
+	return CheckExecutionTime(prev_time, period);
 }
 
-void SaveSatTimeInFRAM(unsigned int time_addr, unsigned int time_size) // Fixed by Maor & Yanir
+void SaveSatTimeInFRAM(unsigned int time_addr, unsigned int time_size)
 {
 	time_unix current_time;
-
-	weak_assert(Time_getUnixEpoch(&current_time) == 0,
-			"Error in SaveSatTimeInFRAM: Can't load the current time!\n");
-
-	weak_assert(FRAM_write((unsigned char *)&current_time, time_addr, time_size) == 0,
-			"Error in SaveSatTimeInFRAM: Can't save the current time in the FRAM!\n");
+	Time_getUnixEpoch(&current_time);
+	FRAM_write(&current_time, time_addr, time_size);
 }
 
-Boolean IsFS_Corrupted() // Fixed by Maor & Yanir
+Boolean IsFS_Corrupted()
 {
-	// Attempt to read from a random place in the memory.
-	// If you succeed, the file system is OK, otherwise it's corrupted.
+	/*
+		Attempt to read from a random place in the memory.
+		If you succeed, the file system is OK, otherwise it's corrupted.
+	*/
 
-	// We randomly chose to read the first activation flag of the satellite, although it doesn't matter.
-	unsigned char dummy_buffer[FIRST_ACTIVATION_FLAG_SIZE];
+	// We randomly chose to read a random property that's stored in the FRAM.
+	unsigned char dummy_buffer[RESET_CMD_FLAG_SIZE];
 
 	// Return TRUE if the read has failed (so it's corrupted), FALSE otherwise.
-	return FRAM_read(dummy_buffer, FIRST_ACTIVATION_FLAG_ADDR, FIRST_ACTIVATION_FLAG_SIZE) != 0;
+	return FRAM_read(dummy_buffer, RESET_CMD_FLAG_ADDR, RESET_CMD_FLAG_SIZE);
 }
 
-int WakeupFromResetCMD() // Done by Blank, Fixed by Maor & Yanir
-{
-	time_unix current_time; // Current time received from function Time_getUnixEpoch
-
-	*(RESET_CMD_FLAG_ADDR) = 0; // Lower the reset flag
-
-	if (weak_assert(Time_getUnixEpoch(&current_time) != 1,
-			"Error in WakeupFromResetCMD: Unix time isn't initialized!\n"))
-	{
-		return SendAckPacket(ACK_RESET_WAKEUP, NULL, (unsigned char *)&current_time, sizeof(time_unix));
-	}
-	return E_NOT_INITIALIZED;
-}
-
+// TODO: Check this, seems pretty damn short
 void ResetGroundCommWDT() // Done by Blank
 {
 	WDT_forceKick(); // Force "kicking" the watchdog timer
 }
 
-Boolean IsGroundCommunicationWDTKick() // Fixed by Maor & Yanir
+Boolean IsGroundCommunicationWDTKick()
 {
 	time_unix current_time;
+	Time_getUnixEpoch(&current_time);
+
 	time_unix last_communication_time;
-
-	weak_assert(Time_getUnixEpoch(&current_time) == 0,
-				"Error in IsGroundCommunicationWDTKick: Can't get the current time!\n");
-
-	weak_assert(FRAM_read((unsigned char*)&last_communication_time, LAST_COMM_TIME_ADDR, LAST_COMM_TIME_SIZE) == 0,
-			"Error in IsGroundCommunicationWDTKick: Can't load the last communication time from the FRAM!\n");
+	FRAM_read(&last_communication_time, LAST_COMM_TIME_ADDR, LAST_COMM_TIME_SIZE);
 
 	// Return TRUE if the time passed since the last communication has exceeded the maximum time, FALSE otherwise.
 	return current_time - last_communication_time >= DEFAULT_NO_COMM_WDT_KICK_TIME;
 }
 
-//TODO: add to command dictionary
-int SetGsWdtKickTime(time_unix new_gs_wdt_kick_time) // Fixed by Maor & Yanir
+int SetGsWdtKickTime(time_unix new_gs_wdt_kick_time)
 {
-	// TODO: Why do you need to check the new kick time? Why 1-2 days?
-	if (new_gs_wdt_kick_time > SECONDS_IN_1_DAY * 2 || new_gs_wdt_kick_time < SECONDS_IN_1_DAY) // watch dog timer between one to two days
-	{
-		return -3; // TODO: Why return -3?
-	}
-
-	return FRAM_write((unsigned char *)&new_gs_wdt_kick_time, NO_COMM_WDT_KICK_TIME_ADDR, NO_COMM_WDT_KICK_TIME_SIZE);
+	return FRAM_write(&new_gs_wdt_kick_time, NO_COMM_WDT_KICK_TIME_ADDR, NO_COMM_WDT_KICK_TIME_SIZE);
 }
 
-time_unix GetGsWdtKickTime() // Fixed by Maor & Yanir
+time_unix GetGsWdtKickTime()
 {
 	time_unix gs_wdt_kick_time;
-
-	weak_assert(FRAM_read((unsigned char *)&gs_wdt_kick_time, NO_COMM_WDT_KICK_TIME_ADDR, NO_COMM_WDT_KICK_TIME_SIZE) == 0,
-			"Error in GetGsWdtKickTime: Can't load the ground station watch dog kick time from the FRAM!\n");
-
+	FRAM_read(&gs_wdt_kick_time, NO_COMM_WDT_KICK_TIME_ADDR, NO_COMM_WDT_KICK_TIME_SIZE);
 	return gs_wdt_kick_time;
 }
 
+int WakeupFromResetCMD()
+{
+	// Lower the reset flag
+	unsigned char data[1] = { 0 }; // The flag is either 0 or 1.
+	int err = FRAM_write(data, RESET_CMD_FLAG_ADDR, RESET_CMD_FLAG_SIZE);
+	if (err) 
+	{
+		printf("Error in WakeupFromResetCMD: Can't lower the reset flag!\n");
+		return err; // Not sure what error code from hal/errors.h is suitable here.
+	}
+
+	// Load the current time
+	time_unix current_time;
+	err = Time_getUnixEpoch(&current_time);
+	if (err)
+	{
+		printf("Error in WakeupFromResetCMD: Unix time isn't initialized!\n");
+		return E_NOT_INITIALIZED;
+	}
+
+	return SendAckPacket(ACK_RESET_WAKEUP, NULL, &current_time, sizeof(time_unix));
+}
+
+// TODO: Do this!
 void Maintenance()
 {
-	SaveSatTimeInFRAM(/*unsigned int time_addr, unsigned int time_size*/);
+	// SaveSatTimeInFRAM(/*unsigned int time_addr, unsigned int time_size*/);
 
-	WakeupFromResetCMD();  // Returns int
+	// WakeupFromResetCMD();  // Returns int
 
-	IsFS_Corrupted();  // Returns boolean
+	// IsFS_Corrupted();  // Returns boolean
 
-	IsGroundCommunicationWDTKick();  // Returns boolean
+	// IsGroundCommunicationWDTKick();  // Returns boolean
 }
